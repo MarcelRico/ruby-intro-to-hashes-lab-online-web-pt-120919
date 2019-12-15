6f4b2c06374445d7535d210553b88c8165e6c5d5
Marcel Rico <personal.mr4@gmail.com>
2019-12-15 13:42:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,6 +29,8 @@ def monopoly_with_third_tier
   monopoly_hash[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
   monopoly_hash[:railroads][:rent_in_dollars][:names][:reading_railroad] = {}
   monopoly_hash[:railroads][:rent_in_dollars][:names][:pennsylvania_railroad] = {}
+  monopoly_hash[:railroads][:rent_in_dollars][:names][:b_and_o_railroad] = {}
+  monopoly_hash[:railroads][:rent_in_dollars][:names][:shortline] = {}
   monopoly_hash
 end
 

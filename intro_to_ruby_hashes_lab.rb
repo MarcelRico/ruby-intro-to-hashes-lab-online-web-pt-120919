9b5b2ee8dc8eea037024b4ecc58068e6b74f2245
Marcel Rico <personal.mr4@gmail.com>
2019-12-15 13:39:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,9 +22,9 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly = monopoly_with_second_tier
-  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
-  monopoly
+  monopoly_hash = monopoly_with_second_tier
+  monopoly_hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  monopoly_hash
 end
 
 def monopoly_with_fourth_tier

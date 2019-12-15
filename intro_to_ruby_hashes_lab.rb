8402dc4a8396336b32b297e9c9af5cfa022326e1
Marcel Rico <personal.mr4@gmail.com>
2019-12-15 13:57:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,5 +40,6 @@ def monopoly_with_fourth_tier
   monopoly_hash = monopoly_with_third_tier
   
   monopoly_hash[:railroads][:names][:reading_railroad]["mortgage_value"] = '$100'
+    monopoly_hash[:railroads][:names][:reading_railroad]["mortgage_value"] = '$100'
 
 end

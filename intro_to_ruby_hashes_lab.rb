98340f5cc0622cc6fe8a4687180e4a6cd7fc98b6
Marcel Rico <personal.mr4@gmail.com>
2019-12-15 13:37:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,7 @@ end
 
 def monopoly_with_third_tier
   monopoly = monopoly_with_second_tier
-  monopoly[:railroads] = {}
-  monopoly[:railroads][:pieces] = 4
-  monopoly[:railroads][:names] = {}
-  monopoly[:railroads][:rent_in_dollars] = {}
+
   monopoly
 end
 
